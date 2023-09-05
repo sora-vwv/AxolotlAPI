ca5de968676e3a5113a7220d39ae75183c10dd4b
Anton Kochergin <87330809+sora-vwv@users.noreply.github.com>
2023-09-05 21:24:20 +0400

    Update Metadata.java

--- src/axl/Metadata.java
+++ src/axl/Metadata.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/*
+
+Метаданные для компилятора Axolotl.
+Хранят информацию про данные и методы,
+которые нет возможности реализовать на
+простом Java.
+
+*/
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface Metadata {
